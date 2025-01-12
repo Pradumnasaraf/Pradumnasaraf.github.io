6e26cb4713fe8ab798776402e2d7363d4da71e4e
Pradumna Saraf <pradumnasaraf@gmail.com>
2025-01-12 17:08:41 +0530

    feat: Add payment documentation for PayPal and Wise, and create a new payment page component

---
+++ my-next-app/src/app/services/payment/page.js
@@ -0,0 +1,75 @@
+'use client';
+import React, { useEffect } from 'react';
+
+const Page = () => {
+  useEffect(() => {
+    // Dynamically load Docsify and Google Tag Manager
+    const scriptDocsify = document.createElement('script');
+    scriptDocsify.src = '//cdn.jsdelivr.net/npm/docsify@4';
+    scriptDocsify.async = true;
+    document.body.appendChild(scriptDocsify);
+
+    const scriptGTM = document.createElement('script');
+    scriptGTM.innerHTML = `
+      (function(w, d, s, l, i) {
+        w[l] = w[l] || []; 
+        w[l].push({
+          'gtm.start': new Date().getTime(),
+          event: 'gtm.js'
+        });
+        var f = d.getElementsByTagName(s)[0],
+          j = d.createElement(s), 
+          dl = l != 'dataLayer' ? '&l=' + l : ''; 
+        j.async = true; 
+        j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl; 
+        f.parentNode.insertBefore(j, f);
+      })(window, document, 'script', 'dataLayer', 'GTM-WRGLMZCX');
+    `;
+    document.head.appendChild(scriptGTM);
+  }, []);
+
+  return (
+    <>
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="description" content="Pradumna Saraf Payments Process" />
+        <link
+          rel="icon"
+          href="https://user-images.githubusercontent.com/51878265/194138074-7a341083-e80e-49d9-8e58-02882b26d3d9.png"
+        />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <meta charSet="UTF-8" />
+        <link
+          rel="stylesheet"
+          href="//cdn.jsdelivr.net/npm/docsify@4/themes/vue.css"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.$docsify = {
+              hideSidebar: true,
+              relativePath: true
+            };
+          `,
+          }}
+        />
+      </head>
+      <body style={{ margin: 0, height: '100vh', overflow: 'auto' }}>
+        {/* Google Tag Manager noscript */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WRGLMZCX"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+
+        <div id="app" style={{ height: '100%', overflowY: 'auto' }}></div>
+      </body>
+    </>
+  );
+};
+
+export default Page;
---
+++ my-next-app/src/app/services/payment/paypal.md
@@ -0,0 +1,19 @@
+## PayPal Payments
+
+**Procedure for Calculating Transfer Fees**
+
+1) To calculate the fees, visit [https://smallseotools.com/paypal-fee-calculator/](https://smallseotools.com/paypal-fee-calculator/) and locate the section shown in the image below. This PayPal calculator is the most accurate one I've found so far. I have encountered inaccuracies with others in the past, as they don't offer flexibility in choosing the Fee Rate.
+
+<p align="center">
+<img src="./paypal-1.png" width="900">
+</p>
+
+2) Enter the amount in USD, in this case, $1000, and select the Fee Rate as 6.5% + $0.05 USD. Then, click on Calculate. This is the current fee rate for international micro payments. You can also verify the fee rate on the [PayPal website](https://www.paypal.com/in/webapps/mpp/merchant-fees) under Micropayments section. You will also find information in the dropdown menu. If the calculator is not working, please contact Pradumna.
+
+<p align="center">
+<img src="./paypal-2.png" width="900">
+</p>
+
+3) After clicking Calculate, you will see the amount to send under "You should ask for," which, in this case, is $1069.57 USD. This is the amount you should transfer to the PayPal account.
+
+If you have any questions, please contact Pradumna at pradumnasaraf@gmail.com.
