4268d47e2bfae9db48dcb2edc788d3b28c85f64f
Pradumna Saraf <pradumnasaraf@gmail.com>
2025-05-08 14:26:08 +0530

    refactor: centralize metadata management and update image URLs
    - Moved metadata definitions to a dedicated file for better organization and maintainability.
    - Updated Open Graph and Twitter image URLs across multiple pages to ensure consistency.
    - Enhanced alt text for images to better reflect current roles and contributions.

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@
 import './globals.css'; // Tailwind and global styles
 import Script from 'next/script';
 import { League_Spartan } from 'next/font/google';
+import { metadata } from './metadata'; // Import metadata from metadata.js
 
 const leagueSpartan = League_Spartan({
   weight: ['300','400','500'],
@@ -13,42 +14,6 @@ export const viewport = {
   width: 'device-width',
   initialScale: 1,
   maximumScale: 1,
-};
-
-export const metadata = {
-  title: 'Pradumna Saraf',
-  description: "Developer Advocate, Docker Captain, and Open Source evangelist. Sharing knowledge about cloud-native technologies, DevOps, and software development.",
-  keywords: "Pradumna Saraf, Developer Advocate, Docker Captain, Open Source, Cloud Native, DevOps, Software Development",
-  authors: [{ name: 'Pradumna Saraf' }],
-  creator: 'Pradumna Saraf',
-  publisher: 'Pradumna Saraf',
-  robots: 'index, follow',
-  openGraph: {
-    title: 'Pradumna Saraf',
-    description: "Developer Advocate, Docker Captain, and Open Source evangelist. Sharing knowledge about cloud-native technologies, DevOps, and software development.",
-    url: 'https://pradumnasaraf.dev',
-    siteName: 'Pradumna Saraf',
-    images: [
-      {
-        url: 'https://pradumnasaraf.dev/media/pradumna-saraf-og.png',
-        width: 1200,
-        height: 630,
-        alt: 'Pradumna Saraf - Open Source Developer/DevRel. Owner @rebasemedia',
-      },
-    ],
-    locale: 'en_US',
-    type: 'website',
-  },
-  twitter: {
-    card: 'summary_large_image',
-    title: 'Pradumna Saraf',
-    description: "Developer Advocate, Docker Captain, and Open Source evangelist. Sharing knowledge about cloud-native technologies, DevOps, and software development.",
-    creator: '@pradumna_saraf',
-    images: ['https://pradumnasaraf.dev/media/pradumna-saraf.png'],
-  },
-  verification: {
-    google: 'your-google-site-verification',
-  },
 };
 
 export default function RootLayout({ children }) {
