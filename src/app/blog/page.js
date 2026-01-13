e7ffdaa9c635dafa79018c2e8b904e10d24e60bb
Pradumna Saraf <pradumnasaraf@gmail.com>
2026-01-13 23:20:51 +0530

    style: update back button dimensions and SVG sizes for consistency across blog components

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -19,8 +19,8 @@ export default function BlogPage() {
       >
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          width="20"
-          height="20"
+          width="24"
+          height="24"
           viewBox="0 0 24 24"
           fill="none"
           stroke="currentColor"
